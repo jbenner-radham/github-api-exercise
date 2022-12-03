6ae79f41caa43d019d2deb1ed637ec7da1f75981
James Benner <james.benner@gmail.com>
2022-12-03 15:32:43 -0500

    Switch from Mocha/Chai to Jest

--- test/index.spec.js
+++
@@ -1,8 +0,0 @@
-// import { expect } from 'chai';
-// import greeting from '../lib/index.js';
-
-// describe('greeting', () => {
-//     it('should return "Hello world!"', () => {
-//         expect(greeting()).to.equal('Hello world!');
-//     });
-// });
