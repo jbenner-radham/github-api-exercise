1d3539a46322183d5fc767b804b6e883b8eced4a
James Benner <james.benner@gmail.com>
2022-12-05 15:18:23 -0500

    Add basic 200 status integration tests

--- test/integration/index.spec.js
+++ test/integration/index.spec.js
@@ -1,6 +1,17 @@
-import { describe, expect, it } from '@jest/globals';
+import { describe, expect, it, jest } from '@jest/globals';
 import request from 'supertest';
-import app from '../../lib/index.js';
+
+jest.unstable_mockModule('../../lib/github-api/get-pull-requests.js', () => ({
+    default: async () => []
+}));
+jest.unstable_mockModule(
+    '../../lib/github-api/get-pull-request-commits.js',
+    () => ({
+        default: async () => []
+    })
+);
+
+const { default: app } = await import('../../lib/index.js');
 
 describe('API', () => {
     describe('when not provided a "repositoryUrl" query parameter', () => {
@@ -44,6 +55,29 @@ describe('API', () => {
                         message:
                             'The provided "repositoryUrl" query parameter is invalid.'
                     });
+                    done();
+                });
+        });
+    });
+
+    describe('when provided a valid "repositoryUrl" query parameter', () => {
+        it('returns a 200 status code', done => {
+            request(app)
+                .get(
+                    `/?repositoryUrl=https://github.com/jbenner-radham/status-cod.es`
+                )
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+
+        it('returns an empty array when no results are found', done => {
+            request(app)
+                .get(
+                    `/?repositoryUrl=https://github.com/jbenner-radham/status-cod.es`
+                )
+                .expect('Content-Type', /json/)
+                .then(response => {
+                    expect(response.body).toEqual([]);
                     done();
                 });
         });
